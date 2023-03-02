d09f57104538a5ab4e933fb399364ccc403925a0
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-03-02 21:02:43 +0600

    fetch or get data from API

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// Fetch or get data from API
+const getDataFromAPI = () => {
+  // data or API source
+  const URL = `https://openapi.programming-hero.com/api/ai/tools`;
+  // fetch data
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => console.log(data.data.tools));
+};
+getDataFromAPI();
