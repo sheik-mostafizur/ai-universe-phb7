0f7c0fec030120699c4ae8e5844436b2e90b57ec
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-03-02 21:44:03 +0600

    spinner added when first loading data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 // Fetch or get data from API
 const getDataFromAPI = () => {
+  isLoading(true);
+
   // data or API source
   const URL = `https://openapi.programming-hero.com/api/ai/tools`;
   // fetch data
@@ -9,17 +11,23 @@ const getDataFromAPI = () => {
 };
 getDataFromAPI();
 
+// See 0 to 6 data
 function showData(data) {
   const show_all_data = document.getElementById("show_all_data");
-  const see_more = document.getElementById("see_more");
+  show_all_data.innerHTML = "";
   data &&
     data.slice(0, 6).forEach((singleData) => {
       show_all_data.appendChild(createSingleItem(singleData));
     });
 
+  // see all data eventHandler
+  const see_more = document.getElementById("see_more");
   see_more.addEventListener("click", () => seeMoreData(data));
+
+  isLoading(false);
 }
 
+// see all data
 function seeMoreData(data) {
   const show_all_data = document.getElementById("show_all_data");
   show_all_data.innerHTML = "";
@@ -27,4 +35,13 @@ function seeMoreData(data) {
     data.forEach((singleData) => {
       show_all_data.appendChild(createSingleItem(singleData));
     });
+}
+
+function isLoading(isLoaded) {
+  const loading = document.querySelector("#loading img");
+  if (isLoaded) {
+    loading.classList.remove("d-none");
+  } else {
+    loading.classList.add("d-none");
+  }
 }
