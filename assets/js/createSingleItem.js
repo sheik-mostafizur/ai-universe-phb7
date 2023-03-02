9db1d91321fdea9e30f654d4163ece1d6a79d19f
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-03-02 22:40:30 +0600

    add features

--- assets/js/createSingleItem.js
+++ assets/js/createSingleItem.js
@@ -1,44 +1,43 @@
 // Create single data
 function createSingleItem(data) {
+  const {id, name, image, published_in, features} = data;
+
+  const generateId = `item_${id}`;
   const div = document.createElement("div");
   div.className = "col-4";
   div.innerHTML = `<div class="card p-3">
-  <img src="https://picsum.photos/id/62/1920/1080" class="rounded" alt="...">
+  <img src="${image}" class="rounded" alt="${name}">
   <div class="card-body">
     <h4 class="card-title fw-bold">Features</h4>
     <ol class="list-group ms-3">
-      <li>paragraph-1</li>
-      <li>paragraph-2</li>
-      <li>paragraph-3</li>
+      <li>${features[0]}</li>
+      <li>${features[1]}</li>
+      <li>${features[2]}</li>
     </ol>
     <hr>
     <div class="d-flex align-items-center justify-content-between">
       <div>
-        <h4 class="card-title fw-bold">Features</h4>
-        <p><img src="./assets/icons/calendar.svg" alt="date"> <span>11/01/2022</span></p>
+        <h4 class="card-title fw-bold">${name}</h4>
+        <p><img src="./assets/icons/calendar.svg" alt="date"> <span>${published_in}</span></p>
       </div>
 
       <div>
         <!-- modal btn or clicker -->
         <button class="btn p-1 border border-danger rounded-circle" data-bs-toggle="modal"
-          data-bs-target="#exampleModal">
+          data-bs-target="#${generateId}">
           <img src="./assets/icons/eye-slash-fill.svg" width="28" alt="show modal eye slash fill icon">
         </button>
         <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+        <div class="modal fade" id="${generateId}" tabindex="-1" aria-labelledby="${generateId}Label"
           aria-hidden="true">
           <div class="modal-dialog  modal-dialog-centered">
             <div class="modal-content">
               <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <h1 class="modal-title fs-5" id="${generateId}Label">${name}</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">
                 ...
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
               </div>
             </div>
           </div>
